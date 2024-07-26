77bc75931801eba2f1fc9116d845744dcf6327e8
Victherich <victherich@gmail.com>
2024-07-26 06:12:53 +0100

    BB

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <ContextProvider>
       <React.StrictMode>
-    <App />
+    {/* <App /> */}
   </React.StrictMode>
   </ContextProvider>
   
