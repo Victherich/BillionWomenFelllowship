6c930adbd904a0d23d03774b494af12b1dcc1f00
Victherich <victherich@gmail.com>
2024-07-29 06:19:00 +0100

    BBB

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -8,7 +8,7 @@ const Video = () => {
     <div className="VideoContainer">
       {/* <img src={BottleAncCup} alt="bottle and cup"/> */}
       <video className="VideoPlayer" controls autoPlay loop muted>
-        <source src="/video2.mp4" type="video/mp4" />
+        <source src="/video1.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
       {/* <img src={BottleAncCup2} alt="bottle and cup"/> */}
