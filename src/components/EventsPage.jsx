751a2d83d650eb9640a5e010939efda30f5b7304
Victherich <victherich@gmail.com>
2024-11-29 19:54:28 +0100

    a

--- src/components/EventsPage.jsx
+++ src/components/EventsPage.jsx
@@ -6,9 +6,11 @@ import eImg1 from "../images/eImg1.jpeg"
 import {FaShareAlt} from "react-icons/fa"
 import { useNavigate } from 'react-router-dom';
 import BackButton from './BackButton';
+import MinistersTraining from "../images/MinistersTraining.jpeg"
 
 const events = [
-    { id: 1,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
+    { id: 2,pic:MinistersTraining, },
+    { id: 1,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST', convener:"with: PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: 'Date: 2024-08-24', time:"Time: 10AM", venue:"Venue: 1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
     // { id: 2,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST2', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
   
 ];
@@ -55,20 +57,20 @@ const EventsPage = () => {
         gap:"30px",
         paddingTop:"100px",
         width:"100%"}}>
-            <h2>UPCOMING EVENTS</h2>
+            <h2>EVENTS</h2>
                 {events.map(event => (
                     <div key={event.id} className="event-card" style={{width:"300px"}}>
                         <img src={event.pic} alt="event image" />
                         <h3>{event.title}</h3>
-                        <p>with</p>
+                        <p></p>
                         <p style={{fontWeight:"bold"}}>{event.convener}</p>
                         <br/>
                         <p>{event.description}</p>
                         <br/>
-                        <p><span style={{fontWeight:"500"}}>Date:</span> {event.date}</p>
-                        <p><span style={{fontWeight:"500"}}>Time:</span> {event.time}</p>
+                        <p><span style={{fontWeight:"500"}}></span> {event.date}</p>
+                        <p><span style={{fontWeight:"500"}}></span> {event.time}</p>
                         <br/>
-                        <p><span style={{fontWeight:"500"}}>Venue:</span> {event.venue}</p>
+                        <p><span style={{fontWeight:"500"}}></span> {event.venue}</p>
                         
                         <button onClick={(e) => { e.stopPropagation(); handleShare(event)}} style={{marginLeft:"10px"}} className='BlogDetailShare'>
         <FaShareAlt /> Share
--- src/components/UpcomingEventsComponent.jsx
+++ src/components/UpcomingEventsComponent.jsx
@@ -5,11 +5,13 @@ import '../CSS/UpcomingEventsComponent.css';
 import eImg1 from "../images/eImg1.jpeg"
 import {FaShareAlt} from "react-icons/fa"
 import { useNavigate } from 'react-router-dom';
+import MinistersTraining from "../images/MinistersTraining.jpeg"
 
 const events = [
-    { id: 1,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
-    { id: 2,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
-    { id: 3,pic:eImg1, title: 'PRAYER & BREAKTHROUGH BREAKFAST', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
+    { id: 1,pic:MinistersTraining, title: '', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
+    { id: 2,pic:MinistersTraining, title: '', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
+    { id: 3,pic:MinistersTraining, title: '', convener:"PASTOR SADE KUKU",description: 'For such a time as this...Esther 4:14', date: '2024-08-24', time:"10AM", venue:"1, OPA ARO CLOSE, OFF TUNJI ADEBAYO STREET, BESIDE AP FILLING STATION, FAGBA LAGOS." },
+ 
 ];
 
 const UpcomingEventsComponent = () => {
@@ -61,15 +63,15 @@ const UpcomingEventsComponent = () => {
                     <div key={event.id} className="event-card" >
                         <img src={event.pic} alt="event image"/>
                         <h3>{event.title}</h3>
-                        <p>with</p>
-                        <p style={{fontWeight:"bold"}}>{event.convener}</p>
+                        {/* <p>with</p> */}
+                        {/* <p style={{fontWeight:"bold"}}>{event.convener}</p> */}
                         <br/>
-                        <p>{event.description}</p>
+                        {/* <p>{event.description}</p> */}
                         <br/>
-                        <p><span style={{fontWeight:"500"}}>Date:</span> {event.date}</p>
-                        <p><span style={{fontWeight:"500"}}>Time:</span> {event.time}</p>
+                        {/* <p><span style={{fontWeight:"500"}}>Date:</span> {event.date}</p> */}
+                        {/* <p><span style={{fontWeight:"500"}}>Time:</span> {event.time}</p> */}
                         <br/>
-                        <p><span style={{fontWeight:"500"}}>Venue:</span> {event.venue}</p>
+                        {/* <p><span style={{fontWeight:"500"}}>Venue:</span> {event.venue}</p> */}
                         
                         <button onClick={(e) => { e.stopPropagation(); handleShare(event)}} style={{marginLeft:"10px"}} className='BlogDetailShare'>
         <FaShareAlt /> Share
